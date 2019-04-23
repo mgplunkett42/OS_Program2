42beb972c3d73e21cfb7dbe20d346dc95afd79ac
mgplunkett42 <45184518+mgplunkett42@users.noreply.github.com>
2019-04-22 20:39:04 -0400

    This is what to add to "sysproc.c"

--- mysystemcall.c
+++ mysystemcall.c
@@ -1,9 +1,16 @@
-int 
 
+int 
 sys_mysystemcall(void)
 {
-	printf("These are the even numbers up until 30.");
-	printf("2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30!!!");
+   cprintf("These are the odd numbers up until 30.\n");
+
+   for (int i = 1; i <= 30; i++)
+   {
+	if (i%2)
+        {
+	     cprintf("%d\n", i);
+	}
+   }
 	
-	return 0;
+   return 0;
 }
